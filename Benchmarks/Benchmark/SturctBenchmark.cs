7b5765d77b4c96eb92dd42d1472d3ab877aefdb8
kimjunghun <lapulass@naver.com>
2022-12-23 10:30:40 +0900

    함수 파라미터에서 struct 값 복사 vs ref 벤치마크

--- Benchmarks/Benchmark/SturctBenchmark.cs
+++ Benchmarks/Benchmark/SturctBenchmark.cs
@@ -63,3 +63,72 @@ public class StructEqualBenchmark : BenchmarkBase, IValidate
         }
     }
 }
+
+[Description("값 복사 vs in 비교")]
+public class StructValueCopyVsIn : BenchmarkBase
+{
+    /*
+|    Method |     Mean |    Error |   StdDev | Allocated |
+|---------- |---------:|---------:|---------:|----------:|
+| ValueCopy | 27.22 ns | 0.550 ns | 0.612 ns |         - |
+|        In | 26.54 ns | 0.534 ns | 0.694 ns |         - |    
+    
+|    Method |     Mean |    Error |   StdDev | Allocated |
+|---------- |---------:|---------:|---------:|----------:|
+| ValueCopy | 26.84 ns | 0.425 ns | 0.332 ns |         - |
+|        In | 27.03 ns | 0.540 ns | 0.505 ns |         - |
+    */
+    public readonly record struct TestStruct
+    {
+        public readonly int Value;
+
+        public TestStruct(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public static TestStruct @struct = new(1);
+
+    [Benchmark]
+    public void ValueCopy()
+    {
+        for (int i = 0; i < 100; ++i)
+        {
+            _ValueCopy(@struct);
+        }
+    }
+
+    public int _ValueCopy(TestStruct value)
+    {
+       return value.Value + 1;
+    }
+
+    [Benchmark]
+    public void In()
+    {
+        for (int i = 0; i < 100; ++i)
+        {
+            _In(in @struct);
+        }
+    }
+
+    public int _In(in TestStruct value)
+    {
+        return value.Value + 1;
+    }
+
+    [Benchmark]
+    public void Ref()
+    {
+        for (int i = 0; i < 100; ++i)
+        {
+            _Ref(ref @struct);
+        }
+    }
+
+    public int _Ref(ref TestStruct value)
+    {
+        return value.Value + 1;
+    }
+}
